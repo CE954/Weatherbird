4ec4187d6c29850b10a1a9937d991c232de4f8de
Christian <christianday954@ymail.com>
2023-03-31 21:22:58 -0700

    Add icons

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-// import getIcon from './scripts/icon.js';
+// import getDayIcon from './scripts/icon.js';
 
-//API call test
+//API calls
 let key = "62b8f696194b27ae3d38708afeb4c3cc";
 
 function fetchWeather(city) {
@@ -20,10 +20,15 @@ function displayWeather(data) {
     document.querySelector(".description").innerText = capitalize(description);
     document.querySelector(".humidity").innerText = `Humidity: ${humidity}%`;
     document.querySelector(".wind").innerText = `Wind Speed: ${speed} MPH`;
+    if (icon.includes("d")) {
+        document.querySelector(".image img").src = getDayIcon(icon);
+    } else {
+        document.querySelector(".image img").src = getNightIcon(icon);
+    }
 }
 
 function capitalize(string) { 
-    str = string.split(" ");
+    let str = string.split(" ");
     let newSent = str.map(word => word.charAt(0).toUpperCase() + word.slice(1));
     return newSent.join(" ");
 }
@@ -33,6 +38,7 @@ console.log("Hello World!");
 window.fetchWeather = fetchWeather;
 window.displayWeather = displayWeather;
 
+// Search button
 let searchForm = document.querySelector(".search");
 
 searchForm.addEventListener("submit", function(event) {
@@ -42,10 +48,56 @@ searchForm.addEventListener("submit", function(event) {
     document.querySelector(".search-bar").value = "";
 })
 
-// let test = document.querySelector(".image");
-// let image = document.createElement("img");
-// image.src = '/images/day.svg';
-// test.appendChild(image)
+// Get icons
+function getDayIcon(icon) {
+    switch (icon) {
+        case "01d":
+            return "src/images/day.svg";
+        case "02d":
+            return "src/images/cloudy-day-1.svg";
+        case "03d":
+            return "src/images/cloudy-day-2.svg";
+        case "04d":
+            return "src/images/cloudy.svg";
+        case "09d":
+            return "src/images/rainy-4.svg";
+        case "10d":
+            return "src/images/rainy-6.svg";
+        case "11d":
+            return "src/images/thunder.svg";
+        case "13d":
+            return "src/images/snowy-6.svg";
+        case "50d":
+            return "src/images/cloudy.svg";
+    }
+}
+
+function getNightIcon(icon) {
+    switch (icon) {
+        case "01n":
+            return "src/images/night.svg";
+        case "02n":
+            return "src/images/cloudy-night-1.svg";
+        case "03n":
+            return "src/images/cloudy-night-1.svg";
+        case "04n":
+            return "src/images/cloudy-night-1.svg";
+        case "09n":
+            return "src/images/rainy-4.svg";
+        case "10n":
+            return "src/images/rainy-6.svg";
+        case "11n":
+            return "src/images/thunder.svg";
+        case "13n":
+            return "src/images/snowy-6.svg";
+        case "50n":
+            return "src/images/cloudy.svg";
+    }
+}
+
+// testing
+// document.querySelector(".image img").src = "src/images/day.svg"
+
 
 // Error handling example
 // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${id}`)
--- src/scripts/icon.js
+++ src/scripts/icon.js
@@ -1,6 +1,22 @@
-// export function getIcon(icon) {
-//     // switch (icon) {
-//     //     case "01d":
-//     //         return
-//     // }
+// export function getDayIcon(icon) {
+//     switch (icon) {
+//         case "01d":
+//             return "src/images/day.svg";
+//         case "02d":
+//             return "src/images/cloudy-day-1.svg";
+//         case "03d":
+//             return "src/images/cloudy-day-2.svg";
+//         case "04d":
+//             return "src/images/cloudy.svg";
+//         case "09d":
+//             return "src/images/rainy-4.svg";
+//         case "10d":
+//             return "src/images/rainy-6.svg";
+//         case "11d":
+//             return "src/images/thunder.svg";
+//         case "13d":
+//             return "src/images/snowy-6.svg";
+//         case "50d":
+//             return "src/images/cloudy.svg";
+//     }
 // }
