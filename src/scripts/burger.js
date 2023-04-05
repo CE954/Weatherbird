56bbb9e902b62789a410b7de49d90231c42bb270
Christian <christianday954@ymail.com>
2023-04-05 12:50:51 -0700

    Fixing image path 2

--- src/scripts/burger.js
+++ src/scripts/burger.js
@@ -5,10 +5,10 @@ const burger = document.querySelector('.burger');
 export function openMenu() { 
     if (favorite.classList.contains('open')) {
         favorite.classList.remove('open');
-        burger.style.background = "url(/src/images/burger.svg)";
+        burger.style.background = "url(../../images/burger.svg)";
     } else { 
         favorite.classList.add('open');
-        burger.style.background = "url('/src/images/close.svg')";
+        burger.style.background = "url('../../images/close.svg')";
     }
 };
 
--- src/scripts/icon.js
+++ src/scripts/icon.js
@@ -5,24 +5,24 @@ function getDayIcon(icon) {
     switch (icon) {
         case "01d":
             // scene.style.background = "url(/src/images/PartlyCloudy.svg)";
-            return "src/images/day.svg";
+            return "../../images/day.svg";
         case "02d":
-            return "src/images/cloudy-day-1.svg";
+            return "../../images/cloudy-day-1.svg";
         case "03d":
-            return "src/images/cloudy-day-2.svg";
+            return "../../images/cloudy-day-2.svg";
         case "04d":
-            // scene.style.background = "url(/src/images/SeaOfClouds.svg)";
-            return "src/images/cloudy.svg";
+            // scene.style.background = "url(/../../images/SeaOfClouds.svg)";
+            return "../../images/cloudy.svg";
         case "09d":
-            return "src/images/rainy-4.svg";
+            return "../../images/rainy-4.svg";
         case "10d":
-            return "src/images/rainy-6.svg";
+            return "../../images/rainy-6.svg";
         case "11d":
-            return "src/images/thunder.svg";
+            return "../../images/thunder.svg";
         case "13d":
-            return "src/images/snowy-6.svg";
+            return "../../images/snowy-6.svg";
         case "50d":
-            return "src/images/cloudy.svg";
+            return "../../images/cloudy.svg";
     }
 }
 
@@ -30,26 +30,26 @@ function getNightIcon(icon) {
     switch (icon) {
         case "01n":
             // scene.style.background = "url(/src/images/NightOfClouds.svg)";
-            return "src/images/night.svg";
+            return "../../images/night.svg";
         case "02n":
-            // scene.style.background = "url(/src/images/NightOfClouds.svg)";
-            return "src/images/cloudy-night-1.svg";
+            // scene.style.background = "url(/../../images/NightOfClouds.svg)";
+            return "../../images/cloudy-night-1.svg";
         case "03n":
-            // scene.style.background = "url(/src/images/NightOfClouds.svg)";
-            return "src/images/cloudy-night-1.svg";
+            // scene.style.background = "url(/../../images/NightOfClouds.svg)";
+            return "../../images/cloudy-night-1.svg";
         case "04n":
-            // scene.style.background = "url(/src/images/SeaOfClouds.svg)";
-            return "src/images/cloudy-night-1.svg";
+            // scene.style.background = "url(/../../images/SeaOfClouds.svg)";
+            return "../../images/cloudy-night-1.svg";
         case "09n":
-            return "src/images/rainy-4.svg";
+            return "../../images/rainy-4.svg";
         case "10n":
-            return "src/images/rainy-6.svg";
+            return "../../images/rainy-6.svg";
         case "11n":
-            return "src/images/thunder.svg";
+            return "../../images/thunder.svg";
         case "13n":
-            return "src/images/snowy-6.svg";
+            return "../../images/snowy-6.svg";
         case "50n":
-            return "src/images/cloudy.svg";
+            return "../../images/cloudy.svg";
     }
 }
 
